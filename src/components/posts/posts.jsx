ef2b643144beecbc235e1453159b0f9e5945ea52
Norris Kwan <norris.kwan@ext.airbnb.com>
2018-09-28 10:10:05 -0500

    refactor posts.jsx

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -21,34 +21,34 @@ class Posts extends Component {
             .then(() => this.setState({ loading: false}));
     }
 
-    componentWillUnmount() {
-        
-    }
-
     render() {
         const { results, pins } = this.props;
 
         if (this.state.loading) return <Loading />;
-        
-        return ( 
+
+        return (
             <div className='posts container'>
-                <ul className="list">
-                    <li>
-                        <div className="fave">
-                            <i class="pin fas fa-heart"></i>
-                        </div>
-                        <div className="item-details">
-                            <span className="title"><strong>Title</strong></span>
-                            <span className="author"><strong>Author</strong></span>
-                            <span className="score"><strong>Score</strong></span>
-                        </div>
-                    </li>
-                </ul>
+                {listHeader}
                 <List name="favorites" items={pins} />
                 <List name="results" items={results} />
             </div>
         );
     }
 }
+
+const listHeader = (
+    <ul className="list">
+        <li>
+            <div className="fave">
+                <i className="pin fas fa-heart"></i>
+            </div>
+            <div className="item-details">
+                <span className="title"><strong>Title</strong></span>
+                <span className="author"><strong>Author</strong></span>
+                <span className="score"><strong>Score</strong></span>
+            </div>
+        </li>
+    </ul>
+  );
 
 export default Posts;
